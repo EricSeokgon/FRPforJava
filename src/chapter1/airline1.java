c7ae0ec20a1c52c7af9b96543902753c9a089ea2
EricSeokgon <leesk55@gmail.com>
2022-03-04 15:15:44 +0900

    sodium 패키지

---
+++ src/chapter1/airline1.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+import javax.swing.*;
+
+public class airline1 {
+    public static void main(String[] args) {
+        JFrame view = new JFrame("airline1");
+        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
